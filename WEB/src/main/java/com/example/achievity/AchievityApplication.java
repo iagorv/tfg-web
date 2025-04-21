126a236ca2e34b62a2d7b2dd4cdc0895f7216157
iagorv <Iagorive18@gmail.com>
2025-04-21 14:17:58 +0200

    Inicio

---
+++ WEB/src/main/java/com/example/achievity/AchievityApplication.java
@@ -0,0 +1,13 @@
+package com.example.achievity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchievityApplication.class, args);
+    }
+
+}
---
+++ WEB/src/test/java/com/example/achievity/AchievityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achievity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchievityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
