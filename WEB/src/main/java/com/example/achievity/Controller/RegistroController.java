2477aa9602c456b0eeaf98dd0c4a7c0625273712
iagorv <Iagorive18@gmail.com>
2025-04-27 17:03:31 +0200

    Mensaje aviso

--- WEB/src/main/java/com/example/achievity/Controller/RegistroController.java
+++ WEB/src/main/java/com/example/achievity/Controller/RegistroController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class RegistroController {
@@ -22,10 +23,12 @@ public class RegistroController {
     }
 
     @PostMapping("/registro")
-    public String registrarUsuario(RegistroDTO registroDTO, Model model) {
+    public String registrarUsuario(RegistroDTO registroDTO, Model model, RedirectAttributes redirectAttributes) {
         boolean exito = usuarioService.registrar(registroDTO);
 
         if (exito) {
+            model.addAttribute("success", "Cuenta creada con éxito. Por favor, inicia sesión.");
+            redirectAttributes.addFlashAttribute("success", "Cuenta creada con éxito. Por favor, inicia sesión.");
             return "redirect:/login"; // Redirige a login si se registra bien
         } else {
             model.addAttribute("error", "Error al registrar. El correo puede estar en uso.");
