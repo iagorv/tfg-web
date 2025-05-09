a21d14b4a773bc0c0ae7710de4b1ac05d6baac50
iagorv <Iagorive18@gmail.com>
2025-05-09 16:13:24 +0200

    Perfil interfaz

--- WEB/src/main/java/com/example/achievity/Controller/UsuarioController.java
+++ WEB/src/main/java/com/example/achievity/Controller/UsuarioController.java
@@ -7,28 +7,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class JuegosController {
+public class UsuarioController {
 
-    private final JuegoApiService juegoApiService;
+
     private final SessionManager sessionManager;
 
-    public JuegosController(JuegoApiService juegoApiService, SessionManager sessionManager) {
-        this.juegoApiService = juegoApiService;
+    public UsuarioController( SessionManager sessionManager) {
+
         this.sessionManager = sessionManager;
     }
-    @GetMapping("/juegos")
-    public String mostrarJuegos(Model model) {
-        model.addAttribute("juegos", juegoApiService.obtenerResumenDeJuegos());
-        return "juegos";
-    }
 
 
-    @GetMapping("/index")
+
+    @GetMapping("/perfil")
     public String mostrarIndex(Model model) {
         if (!sessionManager.estaLogeado()) {
             return "redirect:/login";
         }
-        model.addAttribute("juegos", juegoApiService.obtenerJuegosPopulares());
-        return "index";
+
+        return "perfil";
     }
 }
